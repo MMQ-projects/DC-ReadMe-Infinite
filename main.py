0bac6c45e0eef0d9801de8d211d3d8b8e44bde50
MMQ-projects <332972908+MMQ-projects@users.noreply.github.com>
2026-09-23 18:48:48 +0200

    The code found out what hygiene is.
    Cleared up main.py, and took out the unnecessary lines.
    
    Also made a few changes to make it look less like a code-dump and more like an actual main.py file of the project. Changes include:
    - 3 new functions for previously non-function elements
    - Like an hour of my time
    - That's it.

--- main.py
+++ main.py
@@ -1,52 +1,48 @@
-import random, time
-from time_manager import get_current_time
+import random
 
-with open("progress.txt", "r") as file:
-    percent_value = int(file.read())
+def progress_reader():
+    with open("progress.txt", "r") as file:
+        percent_value = int(file.read())
+    return percent_value
 
+def progress_writer(percent_value):
+    with open("progress.txt", "w") as file:
+        file.write(str(percent_value))
 
-# Idő managelés
-while True:
-     print("Uploading README.txt about user...\n")
-# percent_value = 50 #temporary tesztelési érték
+percent_value = progress_reader()
+print("Uploading README.txt about user...\n")
 
 # A random +-x% ugrás
-     upload_jump = random.randint(-5, 5)
+upload_jump = random.randint(-5, 5)
 
+if upload_jump == 0:
+    print("README.txt has been corrupted. Upload will automatically restart in 24 hours.")
 
-     if upload_jump == 0:
-      print("README.txt has been corrupted. Upload will automatically restart in 24 hours.")
+# A limitek és bounce-back
+else:
+    checking_value = percent_value + upload_jump
 
-    # A limitek és bounce-back
-     else:
-          checking_value = percent_value + upload_jump
+    if checking_value >= 100:
+        print("Overupload error (code 7_OUL). Recovering data from last checkpoint...")
+        recovery_drop = random.randint(5, 7)
+        percent_value = percent_value - recovery_drop
 
-          if checking_value >= 100:
-            print("Overupload error (code 7_OUL). Recovering data from last checkpoint...")
-            recovery_drop = random.randint(5, 7)
-            percent_value = percent_value - recovery_drop
+    elif checking_value <= 0:
+        print("File disappeared error (code 7_FNF). Restarting upload from new directory...")
+        recovery_boost = random.randint(2, 10)
+        percent_value = percent_value + recovery_boost
 
-          elif checking_value <= 0:
-            print("File disappeared error (code 7_FNF). Restartin upload from new directory...")
-            recovery_boost = random.randint(2, 10)
-            percent_value = percent_value + recovery_boost
+    else:
+        percent_value = checking_value
 
-          else:
-               percent_value = checking_value
+    progress_writer(percent_value)
 
+# ASCII feltöltés bar
+def upload_bar(percent_value):
+    bar_length = 25
+    filled = round(bar_length * (percent_value / 100))
+    empty = bar_length - filled
+    bar = "[" + "#" * filled + "-" * empty + "]" + " " + str(percent_value) + "%"
+    return bar
 
-          with open("progress.txt", "w") as file:
-            file.write(str(percent_value)) 
-
-    #print(percent_value)
-
-    # ASCII feltöltés bar
-     bar_length = 25
-     filled = round(bar_length * (percent_value / 100))
-     empty = bar_length - filled
-
-     print("[" + "#"*filled + "-"*empty +"]" + " " + str(percent_value) + "%")
-
-
-   #else: 
-       #print("NO UPDATE YET")
+print(upload_bar(percent_value))
